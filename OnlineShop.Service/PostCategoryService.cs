ca9fdfe347151da623b2887ecdb42573d036da88
DESKTOP-N58044P\admin <hungmien0411@gmail.com>
2021-07-30 18:14:49 +0700

    Commit more service

---
+++ OnlineShop.Service/PostCategoryService.cs
@@ -0,0 +1,62 @@
+﻿using OnlineShop.Data.Infrastructure;
+using OnlineShop.Data.Repositories;
+using OnlineShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Service
+{
+
+    public interface IPostCategoryService
+    {
+        void Add(PostCategories postCategory);
+        void Update(PostCategories postCategory);
+        void Delete(int id);
+        IEnumerable<PostCategories> GetAll();
+        IEnumerable<PostCategories> GetAllByParentId(int parentId);
+        PostCategories getById(int id);
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Add(PostCategories postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategories> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategories> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public PostCategories getById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void Update(PostCategories postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
--- OnlineShop.Service/PostService.cs
+++ OnlineShop.Service/PostService.cs
@@ -16,6 +16,9 @@ namespace OnlineShop.Service
         void Delete(int id);
         IEnumerable<Post> GetAll();
         IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryID,  int page, int pageSize, out int totalRow);
+
         Post GetById(int id);
         IEnumerable<Post> GetAllByTagPaging(string tag , int page, int pageSize, out int totalRow);
         void SaveChanges();
@@ -44,10 +47,15 @@ namespace OnlineShop.Service
             return _postRepository.GetAll(new string[] { "PostCategories" });
         }
 
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryID, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryID, out totalRow, page, pageSize , new string[] { "PostCategories" });
+        }
+
         public IEnumerable<Post> GetAllByTagPaging(string tag , int page, int pageSize, out int totalRow)
         {
             //Todo : Select all post by tag
-            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
         }
 
         public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
